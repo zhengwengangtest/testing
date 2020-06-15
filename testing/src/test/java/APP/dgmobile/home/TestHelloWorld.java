54004aa8e7baf71e6417f0d6f921ebc3e760e477
wb-zwg627607 <wb-zwg627607@alibaba-inc.com>
2020-06-15 11:46:37 +0800

    firt test

---
+++ testing/src/test/java/APP/dgmobile/home/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package APP.dgmobile.home;
+import org.testng.annotations.Test;
+
+/**
+ * @Author: zwg
+ * @Date: 2020/06/15
+ */
+public class TestHelloWorld {
+    @Test
+    public void firstTest (){
+        System.out.println("First test");
+    }
+}
